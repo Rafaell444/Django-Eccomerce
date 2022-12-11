e080145745ad1ac916258ace6f7581bfd2646c79
Rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2022-12-11 13:01:09 +0400

    cart func done

--- store/views.py
+++ store/views.py
@@ -8,7 +8,17 @@ def store(request):
 
 
 def cart(request):
-    return render(request, 'store/cart.html', context={})
+    # check if user authenticated
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    return render(request, 'store/cart.html', context={'items': items, 'order': order})
 
 
 def checkout(request):
