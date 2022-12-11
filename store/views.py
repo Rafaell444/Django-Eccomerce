de97f364840c4d1f3caa6f8e6e8c949ecd6f5ced
Rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2022-12-11 13:05:22 +0400

    checkout page done

--- store/views.py
+++ store/views.py
@@ -22,4 +22,11 @@ def cart(request):
 
 
 def checkout(request):
-    return render(request, 'store/checkout.html', context={})
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    return render(request, 'store/checkout.html', context={'items': items, 'order': order})
